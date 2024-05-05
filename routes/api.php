71ce7db79f16804e1ab1ae198b08da2b7a011656
mahmouderfan95 <mahmoudesmailgamal0@gmail.com>
2024-05-05 17:21:30 +0300

    wedding halls apis

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\WeddingHallCategoryController;
-use App\Http\Controllers\API\WeddingHallController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\WeddingHallCategoryController;
+use App\Http\Controllers\Api\WeddingHallController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
