73ee91f147502aeef59b9afe4b7c9cad4718e2cf
mahmouderfan95 <mahmoudesmailgamal0@gmail.com>
2024-05-15 15:45:24 +0300

    add profit in dashboard page

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -75,7 +75,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-xl-6 col-lg-6 col-12">
+                    <div class="col-xl-4 col-lg-6 col-12">
                         <div class="card pull-up">
                             <div class="card-content">
                                 <div class="card-body">
@@ -97,7 +97,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-xl-6 col-lg-6 col-12">
+                    <div class="col-xl-4 col-lg-6 col-12">
                         <div class="card pull-up">
                             <div class="card-content">
                                 <div class="card-body">
@@ -106,6 +106,28 @@
                                             <div class="media-body text-left">
                                                 <h3 class="success">{{ \App\Models\WeddingHallBooking::count() }}</h3>
                                                 <h6>عدد الحجوزات</h6>
+                                            </div>
+                                            <div>
+                                                <i class="icon-docs info font-large-2 float-right"></i>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    {{-- <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+                                        <div class="progress-bar bg-gradient-x-success" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div> --}}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-6 col-12">
+                        <div class="card pull-up">
+                            <div class="card-content">
+                                <div class="card-body">
+                                    <div class="media d-flex">
+                                        <a href="{{ route('wedding-halls-booking.index') }}">
+                                            <div class="media-body text-left">
+                                                <h3 class="success">{{ \App\Models\WeddingHallBooking::sum('profit') . 'ج م ' }}</h3>
+                                                <h6>مجموع الارباح</h6>
                                             </div>
                                             <div>
                                                 <i class="icon-docs info font-large-2 float-right"></i>
